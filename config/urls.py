c9b4580a500441b0160d55a30ce41a94d9db0969
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2024-11-27 19:43:52 +0900

    amend urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from act_Main.views import home
-
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]

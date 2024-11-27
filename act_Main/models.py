9958ca7166f136a804edf067c8e0c904b60b71a2
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2024-11-27 19:42:59 +0900

    Delete act_main Code

--- act_Main/models.py
+++ act_Main/models.py
@@ -1,30 +1,29 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=10)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Recruit(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    post_at = models.DateTimeField(auto_now_add=True)
-    # closing_date = models.DateField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    self_introduction = models.CharField(max_length=255)
-    portfolio = models.ImageField(upload_to="portfolio", null=True, blank=True)
-
-    def __str__(self):
-        return self.user
+# class Category(models.Model):
+#     name = models.CharField(max_length=10)
+#     slug = models.SlugField(unique=True)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Recruit(models.Model):
+#     title = models.CharField(max_length=255)
+#     description = models.CharField(max_length=255)
+#     post_at = models.DateTimeField(auto_now_add=True)
+#     # closing_date = models.DateField()
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.title
+#
+#
+# class Profile(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     self_introduction = models.CharField(max_length=255)
+#     portfolio = models.ImageField(upload_to="portfolio", null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.user
--- act_Main/urls.py
+++ act_Main/urls.py
@@ -1,17 +1,16 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    # Main_Page 경로
-    path("", views.home, name="home"),
-    path("category/<slug:category_slug>/", views.category, name="category"),
-    # Category
-    path("category/<slug:category_slug>/", views.category_list, name="category_list"),
-    path(
-        "category/<slug:category_slug>/", views.category_detail, name="category_detail"
-    ),
-    path("add-category/", views.add_category, name="add_category"),
-]
+# urlpatterns = [
+
+# Main_Page 경로
+#     path("", views.home, name="home"),
+#     path("category/<slug:category_slug>/", views.category, name="category"),
+#     # Category
+#     path("category/<slug:category_slug>/", views.category_list, name="category_list"),
+#     path(
+#         "category/<slug:category_slug>/", views.category_detail, name="category_detail"
+#     ),
+#     path("add-category/", views.add_category, name="add_category"),
+# ]
